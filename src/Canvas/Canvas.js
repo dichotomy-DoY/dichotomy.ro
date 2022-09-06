f3f1ae4d8ba0a4cc6876ee0688af5fa67b28f389
Saad Bilal <saad103bilal@gmail.com>
2022-09-06 13:43:07 +0300

    ParticlesJs config updated

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -67,6 +67,118 @@ const App = () => {
       });
   }, []);
 
+  useEffect(()=>{
+    window.tsParticles.load("tsparticles", {
+      "particles": {
+        "number": {
+          "value": 50,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.8979107540846928,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 4.5,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": false,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": false,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      }});
+  }, []);
+
   // useEffect(()=>{
   //   if(videoElement && videoElement.current) {
   //     videoElement.current.addEventListener('canplaythrough', resourceLoaded, {once:true});
@@ -89,7 +201,9 @@ const App = () => {
   };
 
   return (
-    <div 
+    <div
+      className={`${isLoading ? '' : 'main-container'}`}
+      id="tsparticles"
       ref={canvasElement}
       style={{
         height: "100%"
@@ -152,23 +266,7 @@ const App = () => {
                       clickActionObject={obj.clickActionObject}
                     />
                   </a>
-                ) : (obj.type === "Video" && !obj.hideImg ? (
-                  <video
-                    key={obj.key}
-                    src={obj.imgUrl}
-                    style={{
-                      position: "absolute",
-                      width: obj.width,
-                      height: obj.height,
-                      top: obj.top,
-                      left: obj.left,
-                      objectFit: "cover",
-                    }}
-                    autoPlay
-                    loop
-                    muted
-                  />
-                ) : '')}
+                ) : (obj.type === "Video" && !obj.hideImg ? ('') : '')}
               </>
             ))}
           </div>
